faa4c683b43420fd5dfa9122f974ef8ceb4a8706
wangbin <wangbin@gmail.com>
2011-07-28 13:39:19 +0800

    changed version 9.3

--- javaDemo/src/com/javaDemo/demo/User.java
+++ javaDemo/src/com/javaDemo/demo/User.java
@@ -87,5 +87,13 @@ public class User {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	
 }
